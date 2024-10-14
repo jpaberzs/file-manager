db0e39f10b1c3be15ac8a323fda1b7605dfcfaee
jpaberzs <janis.paberzs18@gmail.com>
2024-10-15 01:55:03 +0300

    fix: fixed main branch

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## File manager
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/jpaberzs/file-manager.git
+```
+
+2. To get final version of project checkout to develop branch
+
+```bash
+git checkout develop
+```
--- handlers/indexHandler.js
+++
@@ -1,9 +0,0 @@
-export const indexHandler = async (line, readline) => {
-  const [command, ...args] = line.trim().split(/\s+/g);
-
-  console.log("Your command is: " + command);
-
-  if (line === ".exit") {
-    readline.close();
-  }
-};
